c0c81ba02021814d36ba3b43a8013a4d79bd429a
David North <bandanna2k@gmail.com>
2024-12-07 22:32:39 +1300

    Advent of Code - Day 7 part 2

--- 2024/Day 7/Part2/Tests.py
+++ 2024/Day 7/Part2/Tests.py
@@ -6,9 +6,9 @@ class Tests(unittest.TestCase):
 
     def test_the_test_input(self):
         test = Processor('test_input.txt')
-        self.assertEqual(test.getAnswer(), 3749)
+        self.assertEqual(test.getAnswer(), 11387)
 
     def test_the_real_input(self):
         test = Processor('real_input.txt')
-        self.assertEqual(test.getAnswer(), 1298103531759)
+        self.assertEqual(test.getAnswer(), 140575048428831)
 
