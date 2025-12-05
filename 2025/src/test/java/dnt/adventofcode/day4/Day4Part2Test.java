d0d1cac12b77f477237a49e886ffca1323132860
David North <bandanna2k@gmail.com>
2025-12-05 14:37:42 +1300

    Advent of Code 2025, Day 4 Done

--- 2025/src/test/java/dnt/adventofcode/day4/Day4Part2Test.java
+++ 2025/src/test/java/dnt/adventofcode/day4/Day4Part2Test.java
@@ -9,29 +9,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Day4Part1Test extends TestBase
-{
-    private static Stream<Arguments> part1() {
+public class Day4Part2Test extends TestBase {
+    private static Stream<Arguments> inputs() {
         return Stream.of(
-                Arguments.of("/day4test.txt", 13),
-                Arguments.of("/day4real.txt", 1505)
+                Arguments.of("/day4test.txt", 43),
+                Arguments.of("/day4real.txt", 9182)
         );
     }
 
     @ParameterizedTest(name = "Expected {1}")
-    @MethodSource("part1")
-    void part1(final String input, final long expected) throws IOException
-    {
+    @MethodSource("inputs")
+    void test(final String input, final long expected) throws IOException {
         final char[][] grid;
         AtomicInteger sizeX = new AtomicInteger();
         AtomicInteger sizeY = new AtomicInteger();
-        try (InputStream inputStream = Day4Part1Test.class.getResourceAsStream(input))
-        {
+        try (InputStream inputStream = Day4Part2Test.class.getResourceAsStream(input)) {
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             bufferedReader.lines().forEach(line ->
@@ -41,8 +40,7 @@ public class Day4Part1Test extends TestBase
             });
             grid = new char[sizeY.get()][sizeX.get()];
         }
-        try (InputStream inputStream = Day4Part1Test.class.getResourceAsStream(input))
-        {
+        try (InputStream inputStream = Day4Part2Test.class.getResourceAsStream(input)) {
             AtomicInteger x = new AtomicInteger();
             AtomicInteger y = new AtomicInteger();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -58,37 +56,46 @@ public class Day4Part1Test extends TestBase
             });
         }
         int totalCount = 0;
-        for (int x = 0; x < sizeX.get(); x++) {
-            for (int y = 0; y < sizeY.get(); y++) {
-                if(grid[x][y] == '@') {
 
-                    int count = 0;
-                    count += (check(grid, x - 1, y - 1) ? 1 : 0);
-                    count += (check(grid, x, y - 1) ? 1 : 0);
-                    count += (check(grid, x + 1, y - 1) ? 1 : 0);
+        Set<int[]> toBeRemoved = new HashSet<>();
+        do {
+            toBeRemoved.forEach(ints -> grid[ints[0]][ints[1]] = ' ');
+            toBeRemoved.clear();
 
-                    count += (check(grid, x - 1, y) ? 1 : 0);
-                    count += (check(grid, x + 1, y) ? 1 : 0);
+            for (int x = 0; x < sizeX.get(); x++) {
+                for (int y = 0; y < sizeY.get(); y++) {
+                    if (grid[x][y] == '@') {
 
-                    count += (check(grid, x - 1, y + 1) ? 1 : 0);
-                    count += (check(grid, x, y + 1) ? 1 : 0);
-                    count += (check(grid, x + 1, y + 1) ? 1 : 0);
+                        int count = 0;
+                        count += (check(grid, x - 1, y - 1) ? 1 : 0);
+                        count += (check(grid, x, y - 1) ? 1 : 0);
+                        count += (check(grid, x + 1, y - 1) ? 1 : 0);
 
-                    if (count < 4) {
-                        grid[x][y] = 'x';
-                        totalCount++;
+                        count += (check(grid, x - 1, y) ? 1 : 0);
+                        count += (check(grid, x + 1, y) ? 1 : 0);
+
+                        count += (check(grid, x - 1, y + 1) ? 1 : 0);
+                        count += (check(grid, x, y + 1) ? 1 : 0);
+                        count += (check(grid, x + 1, y + 1) ? 1 : 0);
+
+                        if (count < 4) {
+                            grid[x][y] = 'x';
+                            toBeRemoved.add(new int[]{x, y});
+                            totalCount++;
+                        }
                     }
                 }
             }
         }
+        while(!toBeRemoved.isEmpty());
+
         assertThat(totalCount).isEqualTo(expected);
     }
 
     private static boolean check(char[][] grid, int x, int y) {
         try {
             return grid[x][y] == '@' || grid[x][y] == 'x';
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
